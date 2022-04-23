7ca363e5fff60ccc111447f5f625df513caae663
anmol-a1 <anmol.kamble2453@gmail.com>
2022-04-23 16:46:55 +0530

    added cinfiguration

--- flamincotech/Admin1/views.py
+++ flamincotech/Admin1/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect,HttpResponse
+from .models import InputConfiguration
 def index(request):
     return render(request,'Admin1/index.html')
 def registeruser(request):
@@ -8,4 +9,9 @@ def history(request):
     return render(request,'Admin1/history.html')
 def trackp(request):
     return render(request,'Admin1/trackp.html')
+def configuration(request):
+    entries=InputConfiguration.objects.all()
+    print(len(entries))
+    context={'entries':entries}
+    return render(request,'Admin1/configuration.html',context)
 # Create your views here.
